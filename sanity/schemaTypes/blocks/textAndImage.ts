d38c15c636775db3b47127ed6ea39fba4c4b5c01
Arnaud De Burchgraeve <99794461+arnaud-dbu@users.noreply.github.com>
2024-09-01 23:08:50 +0200

    First commit

--- sanity/schemaTypes/blocks/textAndImage.ts
+++ sanity/schemaTypes/blocks/textAndImage.ts
@@ -1,66 +1,65 @@
-import {defineField, defineType} from 'sanity'
-import {PiTextTFill} from 'react-icons/pi'
-import title from '../objects/title'
-import richText from '../objects/richText'
-import backgroundColor from '../objects/backgroundColor'
-import showImage from '../objects/showImage'
-import links from '../objects/links'
+import { defineField, defineType } from "sanity";
+import { PiTextTFill } from "react-icons/pi";
+import title from "../objects/title";
+import richText from "../objects/richText";
+import backgroundColor from "../objects/backgroundColor";
+import showImage from "../objects/showImage";
+import links from "../objects/links";
+import tagline from "../objects/tagline";
 
 export default defineType({
-  name: 'textAndImage',
-  title: 'Text & Image',
-  type: 'object',
+  name: "textAndImage",
+  title: "Text / Image",
+  type: "object",
   icon: PiTextTFill,
   preview: {
     select: {
-      title: 'title',
-      image: 'image',
+      title: "title",
+      image: "image",
     },
-    prepare({title, image}) {
+    prepare({ title, image }) {
       return {
-        title: title || 'Untitled',
-        subtitle: 'Text & Image',
+        title: title || "Untitled",
+        subtitle: "Text / Image",
         media: image || PiTextTFill,
-      }
+      };
     },
   },
   fields: [
+    defineField(tagline),
     defineField(title),
     defineField(richText),
     defineField(links),
-    defineField(showImage),
     defineField({
-      name: 'image',
-      type: 'image',
+      name: "image",
+      type: "image",
       options: {
         hotspot: true,
       },
-      hidden: ({parent}: any) => !parent?.showImage,
       fields: [
         {
-          name: 'alt',
-          title: 'Alternative text',
-          type: 'string',
+          name: "alt",
+          title: "Alternative text",
+          type: "string",
           options: {
             isHighlighted: true,
           },
         },
         {
-          name: 'imagePosition',
-          title: 'Image Position',
-          type: 'string',
-          initialValue: 'left',
+          name: "imagePosition",
+          title: "Image Position",
+          type: "string",
+          initialValue: "left",
           options: {
             list: [
-              {title: 'Left', value: 'left'},
-              {title: 'Right', value: 'right'},
+              { title: "Left", value: "left" },
+              { title: "Right", value: "right" },
             ],
-            layout: 'radio',
-            direction: 'horizontal',
+            layout: "radio",
+            direction: "horizontal",
           },
         },
       ],
     }),
-    defineField(backgroundColor),
   ],
-})
+});
