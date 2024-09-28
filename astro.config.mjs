cc40a13fa5daf939de5fdf868c8cd625e08f1554
Arnaud De Burchgraeve <99794461+arnaud-dbu@users.noreply.github.com>
2024-09-28 22:59:08 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   integrations: [
     astroImageTools,
     sanity({
