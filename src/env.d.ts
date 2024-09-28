7f802ab854b729ea557f520e5ec50c55860db4c9
Arnaud De Burchgraeve <99794461+arnaud-dbu@users.noreply.github.com>
2024-09-28 17:56:21 +0200

    Install depedencies

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
+/// <reference types="@stackbit/types" />
+/// <reference types="@stackbit/cms-sanity" />
