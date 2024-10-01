6b8263d5b1468699aa909444de08cb8d2c19756d
Arnaud De Burchgraeve <99794461+arnaud-dbu@users.noreply.github.com>
2024-10-01 23:39:25 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,11 +8,28 @@ import { structure } from "./studio/structure/structure";
 import { presentationTool } from "sanity/presentation";
 import type { PluginOptions } from "sanity";
 
+const projectId =
+  import.meta.env.PUBLIC_SANITY_STUDIO_PROJECT_ID! ||
+  import.meta.env.PUBLIC_SANITY_PROJECT_ID!;
+const dataset =
+  import.meta.env.PUBLIC_SANITY_STUDIO_DATASET! ||
+  import.meta.env.PUBLIC_SANITY_DATASET!;
+
+if (!projectId || !dataset) {
+  throw new Error(
+    `Missing environment variable(s). Check if named correctly in .env file.\n\nShould be:\nPUBLIC_SANITY_STUDIO_PROJECT_ID=${projectId}\nPUBLIC_SANITY_STUDIO_DATASET=${dataset}\n\nAvailable environment variables:\n${JSON.stringify(
+      import.meta.env,
+      null,
+      2
+    )}`
+  );
+}
+
 export default defineConfig({
-  title: "silweb",
-  name: "silweb",
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  title: "Silsomzorg",
+  name: "silsomzorg",
+  projectId,
+  dataset,
   plugins: [
     structureTool({ structure }),
     linkField({
