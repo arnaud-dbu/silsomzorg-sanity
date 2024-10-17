15ff21d3f1d3e2bd067d7fd700397167332ae711
Arnaud De Burchgraeve <99794461+arnaud-dbu@users.noreply.github.com>
2024-10-17 20:50:49 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
     visionTool(),
     media() as PluginOptions,
     presentationTool({
-      previewUrl: "https://silsomzorg.netlify.app",
+      previewUrl: SANITY_STUDIO_PREVIEW_URL,
     }) as PluginOptions,
   ],
   schema: {
